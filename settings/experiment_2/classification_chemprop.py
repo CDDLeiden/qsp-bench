69d05787dcc806f1761369505df627013b4086b9
martin-sicho <sicho.martin@gmail.com>
2024-02-09 17:56:44 +0100

    add chemprop classification

---
+++ settings/experiment_2/classification_chemprop.py
@@ -0,0 +1,26 @@
+from qsprpred.data.descriptors.sets import SmilesDesc
+from qsprpred.extra.gpu.models.chemprop import ChempropModel
+from settings.experiment_2.classification import *
+
+DESCRIPTORS = [
+    [SmilesDesc()],
+]
+
+MODELS = [
+    ChempropModel(
+        name="Chemprop",
+        base_dir=MODELS_DIR,
+    ),
+]
+
+SETTINGS = BenchmarkSettings(
+    name=NAME,
+    n_replicas=N_REPLICAS,
+    random_seed=SEED,
+    data_sources=DATA_SOURCES,
+    descriptors=DESCRIPTORS,
+    target_props=TARGET_PROPS,
+    prep_settings=DATA_PREPS,
+    models=MODELS,
+    assessors=ASSESSORS,
+)
