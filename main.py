a70be83c3701bf210c2ed6f813aaba00a430f247
martin-sicho <sicho.martin@gmail.com>
2024-02-10 12:10:25 +0100

    prevent semaphore leaks by putting imports in main() too

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import logging
-
-import os
-from importlib import import_module
-
-from qsprpred.benchmarks import BenchmarkSettings, BenchmarkRunner
-from qsprpred.logs import logger
-
-logger.setLevel(logging.INFO)
-
-
 def main():
+    import logging
+
+    import os
+    from importlib import import_module
+
+    from qsprpred.benchmarks import BenchmarkSettings, BenchmarkRunner
+    from qsprpred.logs import logger
+
+    logger.setLevel(logging.INFO)
+
     if "QSPBENCH_SETTINGS" not in os.environ:
         raise ValueError(
             "Please set the QSPBENCH_SETTINGS environment "
@@ -29,13 +28,20 @@ def main():
         models=settings_module.MODELS,
         assessors=settings_module.ASSESSORS,
     )
+    if settings_module.RESET_MODELS and os.path.exists(settings_module.MODELS_DIR):
+        logger.warning("Resetting models directory.")
+        import shutil
+
+        shutil.rmtree(settings_module.MODELS_DIR)
+    if settings_module.RESET_MODELS and os.path.exists(settings_module.RESULTS_FILE):
+        logger.warning("Resetting results file.")
+        os.remove(settings_module.RESULTS_FILE)
     runner = BenchmarkRunner(
         settings,
         settings_module.N_PROC,
         settings_module.DATA_DIR,
         settings_module.RESULTS_FILE,
         settings_module.GPUS,
-        models_per_gpu=1,
     )
     runner.run(raise_errors=True)
     print("Benchmark finished.")
